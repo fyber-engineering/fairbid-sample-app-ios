6c3e3fec8b572daa7276e1fad396d90c69a1f9d1
avi gelkop <avi.gelkop@fyber.com>
2019-04-10 15:21:07 +0300

    Integration completed

---
+++ Fyber FairBid/FyberFairBid-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  FyberFairBid-Bridging-Header.h
+//  Fyber FairBid
+//
+//  Created by Avi Gelkop on 4/8/19.
+//  Copyright © 2019 Fyber. All rights reserved.
+//
+
+#ifndef FyberFairBid_Bridging_Header_h
+#define FyberFairBid_Bridging_Header_h
+
+
+#endif /* FyberFairBid_Bridging_Header_h */
